69df81688e33953a206a18f5df69f433b2e34658
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-06-09 00:41:49 +0200

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
 lint = require('gulp-jshint'),
+mocha = require('gulp-spawn-mocha'),
 
 paths = {
-   nodeScripts: 'app/**/*.js'
+   nodeScripts: 'app/**/*.js',
+   testScripts: 'app/tests/unit/server/**/*.js'
 };
 
 gulp.task('lint', function() {
@@ -11,8 +13,23 @@ gulp.task('lint', function() {
       .pipe(lint.reporter('jshint-reporter-jscs'));
 });
 
-gulp.task('watch', function() {
-   gulp.watch(paths.nodeScripts, ['lint'])
+gulp.task('mocha', function() {
+   return gulp.src(paths.testScripts)
+      .pipe(mocha({reporter: 'spec'}))
+      .once('error', function(err) {
+         console.log(err);
+         this.emit('end');
+      });
 });
 
-gulp.task('default', ['watch', 'lint'])
+gulp.task('watch', function() {
+   gulp.watch(paths.nodeScripts, ['lint']);
+});
+
+gulp.task('mocha-watch', function() {
+   gulp.watch(paths.nodeScripts, ['mocha']);
+});
+
+gulp.task('default', ['watch', 'lint']);
+
+gulp.task('test', ['mocha-watch', 'mocha']);
