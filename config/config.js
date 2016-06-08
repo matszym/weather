cda1f4041d2d96e0a5f10180a80c1728fa24c732
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-06-09 00:31:23 +0200

    Added config file sample

--- config/config.js
+++ config/config.js
@@ -1,1 +1,15 @@
 module.exports = require('./env/' + process.env.NODE_ENV + '.js');
+
+/* 
+
+Sample config file:
+
+######################
+env/production.js
+######################
+
+module.exports = {
+   googleMapsAPI: 'your API key'
+}
+
+*/
